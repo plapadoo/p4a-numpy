919908703d980dc3199077f72fc6c59048b8940b
Kim Rinnewitz <krinnewitz@plapadoo.de>
2018-08-28 17:09:58 +0200

    Upgrade numpy version, fix python2

--- recipes/numpy/__init__.py
+++ recipes/numpy/__init__.py
@@ -1,48 +1,55 @@
-import os
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 from pythonforandroid.toolchain import warning
+from os.path import join
 
 
 class NumpyRecipe(CompiledComponentsPythonRecipe):
 
-    version = '1.14.5'
+    version = '1.15.1'
     url = 'https://pypi.python.org/packages/source/n/numpy/numpy-{version}.zip'
-    site_packages_name= 'numpy'
+    site_packages_name = 'numpy'
 
     depends = [('python2', 'python3crystax')]
 
-    recipe_dir = os.path.dirname(os.path.realpath(__file__))
-
-    patches = [os.path.join(recipe_dir, 'patches', 'fix-numpy.patch'),
-               os.path.join(recipe_dir, 'patches', 'prevent_libs_check.patch'),
-               os.path.join(recipe_dir, 'patches', 'ar.patch'),
-               os.path.join(recipe_dir, 'patches', 'lib.patch')
+    patches = [
+        join('patches', 'fix-numpy.patch'),
+        join('patches', 'prevent_libs_check.patch'),
+        join('patches', 'ar.patch'),
+        join('patches', 'lib.patch'),
+        join('patches', 'python2-fixes.patch')
     ]
 
     def get_recipe_env(self, arch):
         env = super(NumpyRecipe, self).get_recipe_env(arch)
 
-        py_ver =self.ctx.python_recipe.version[0:3]
-        py_so = '2.7' if py_ver == '2.7' else py_ver + 'm'
-        api_ver = self.ctx.android_api
-        platform = 'arm' if 'arm' in arch.arch else arch.arch
+        flags = " -L{} --sysroot={}".format(
+            join(self.ctx.ndk_platform, 'usr', 'lib'),
+            self.ctx.ndk_platform
+        )
 
-        flags = " -L{ctx.ndk_dir}/platforms/android-{api_ver}/arch-{platform}/usr/lib/" \
-                " --sysroot={ctx.ndk_dir}/platforms/android-{api_ver}/arch-{platform}" \
-            .format(ctx=self.ctx, arch=arch, platform=platform, api_ver=api_ver)
-
-        if {'python3crystax'}:
-            flags += " -I{ctx.ndk_dir}/sources/crystax/include/" \
-                     " -I{ctx.ndk_dir}/sources/python/{py_ver}/include/python/" \
-                     " -L{ctx.ndk_dir}/sources/crystax/libs/{arch.arch}/" \
-                     " -L{ctx.ndk_dir}/sources/python/{py_ver}/libs/{arch.arch}/ -lpython{py_so}" \
-                .format(ctx=self.ctx, arch=arch, py_so=py_so, py_ver=py_ver)
+        if self.ctx.ndk == 'crystax':
+            py_ver = self.ctx.python_recipe.version[0:3]
+            src_dir = join(self.ctx.ndk_dir, 'sources')
+            py_inc_dir = join(src_dir, 'python', py_ver, 'include', 'python')
+            py_lib_dir = join(src_dir, 'python', py_ver, 'libs', arch.arch)
+            cry_inc_dir = join(src_dir, 'crystax', 'include')
+            cry_lib_dir = join(src_dir, 'crystax', 'libs', arch.arch)
+            flags += ' -I{}'.format(py_inc_dir)
+            flags += ' -L{} -lpython{}m'.format(py_lib_dir, py_ver)
+            flags += " -I{}".format(cry_inc_dir)
+            flags += " -L{}".format(cry_lib_dir)
 
         if flags not in env['CC']:
             env['CC'] += flags
         if flags not in env['LD']:
             env['LD'] += flags + ' -shared'
-
         return env
+
+    def prebuild_arch(self, arch):
+        super(NumpyRecipe, self).prebuild_arch(arch)
+
+        warning('Numpy is built assuming the archiver name is '
+                'arm-linux-androideabi-ar, which may not always be true!')
+
 
 recipe = NumpyRecipe()
